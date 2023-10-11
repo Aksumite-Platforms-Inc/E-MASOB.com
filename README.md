eb32352d6f18243dcb872b30071ee1f0ef4e6b2b
zemuumez <135070468+zemuumez@users.noreply.github.com>
2023-10-11 08:43:27 +0300

    segmented the customer site
    segmented the customer site

--- README.md
+++
@@ -1,2 +0,0 @@
-# E-MASOB.com
-E-MASOB food delivery, ordering and  restaurant management platform 
--- delivery_man/del_man/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DelManConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'del_man'
